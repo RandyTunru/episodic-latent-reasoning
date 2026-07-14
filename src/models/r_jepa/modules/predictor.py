4b25bebb3596e089202ebd56bd431be42c5c7e14
randy <randytunru@gmail.com>
2026-07-14 17:32:39 +0700

    fix: fixed mask and freqs_cis implementation for rjepa predictor

--- src/models/r_jepa/modules/predictor.py
+++ src/models/r_jepa/modules/predictor.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 from src.models.modules.block import SelfAttentionBlock, CrossAttentionBlock, RMSNorm
 
 class RJEPAPredictor(nn.Module, ABC):
-    def __init__(self, encoder_dim, predictor_dim):
+    def __init__(self, encoder_dim, predictor_dim, num_heads, max_seq_length):
         super(RJEPAPredictor, self).__init__()
         self.start_token = nn.Parameter(torch.randn(1, 1, predictor_dim))  # Learnable start token for the predictor
 
@@ -16,18 +16,35 @@ class RJEPAPredictor(nn.Module, ABC):
         self.norm = RMSNorm(predictor_dim)
         self.up_projection = nn.Linear(predictor_dim, encoder_dim)
 
+        # RoPE Frequencies 
+        head_dim = predictor_dim // num_heads
+        freqs = torch.arange(0, head_dim, 2) / head_dim
+        freqs = 1 / (10000 ** freqs)
+
+        t = torch.arange(max_seq_length) 
+
+        angles = torch.outer(t, freqs)  
+        freqs_cis = torch.polar(torch.ones_like(angles), angles)  
+        
+        # Causal Mask 
+        mask = torch.tril(torch.ones(max_seq_length, max_seq_length)).bool().unsqueeze(0).unsqueeze(0)
+        
+        # Register them as buffers to ensure they are moved to the correct device with the model
+        self.register_buffer('freqs_cis', freqs_cis, persistent=False)
+        self.register_buffer('mask', mask, persistent=False)
+
     @abstractmethod
-    def forward(self, x, context=None, mask=None, freqs_cis=None):
+    def forward(self, x, context=None):
         pass
 
 class CrossAttentionPredictor(RJEPAPredictor):
-    def __init__(self, encoder_dim, predictor_dim, num_heads, d_ff, num_layers, dropout=0.0):
-        super(CrossAttentionPredictor, self).__init__(encoder_dim, predictor_dim)
+    def __init__(self, encoder_dim, predictor_dim, num_heads, d_ff, num_layers, max_seq_length, dropout=0.0):
+        super(CrossAttentionPredictor, self).__init__(encoder_dim, predictor_dim, num_heads, max_seq_length)
         self.layers = nn.ModuleList([
             CrossAttentionBlock(predictor_dim, num_heads, d_ff, dropout) for _ in range(num_layers)
         ])
 
-    def forward(self, x, context, mask=None, freqs_cis=None):
+    def forward(self, x, context):
         # Project context to predictor dimension
         context = self.context_down_projection(context) #(B, context_len, predictor_dim)
         # Project input to predictor dimension
@@ -36,20 +53,20 @@ class CrossAttentionPredictor(RJEPAPredictor):
         x = torch.cat([self.start_token.expand(x.size(0), -1, -1), x], dim=1)  # Prepend start token
 
         for layer in self.layers:
-            x = layer(x, context=context, mask=mask, freqs_cis=freqs_cis)
+            x = layer(x, context=context, mask=self.mask, freqs_cis=self.freqs_cis)
         x = self.norm(x)
 
         x = self.up_projection(x)
         return x
 
 class CausalAttentionPredictor(RJEPAPredictor):
-    def __init__(self, encoder_dim, predictor_dim, num_heads, d_ff, num_layers, dropout=0.0):
-        super(CausalAttentionPredictor, self).__init__(encoder_dim, predictor_dim)
+    def __init__(self, encoder_dim, predictor_dim, num_heads, d_ff, num_layers, max_seq_length, dropout=0.0):
+        super(CausalAttentionPredictor, self).__init__(encoder_dim, predictor_dim, num_heads, max_seq_length)
         self.layers = nn.ModuleList([
             SelfAttentionBlock(predictor_dim, num_heads, d_ff, dropout) for _ in range(num_layers)
         ])
 
-    def forward(self, x, context, mask=None, freqs_cis=None):
+    def forward(self, x, context):
         context_len = context.size(1)
 
         # Project context to predictor dimension
@@ -60,7 +77,7 @@ class CausalAttentionPredictor(RJEPAPredictor):
         x = torch.cat([context, self.start_token.expand(x.size(0), -1, -1), x], dim=1)  # Prepend context and start token
 
         for layer in self.layers:
-            x = layer(x, mask=mask, freqs_cis=freqs_cis)  
+            x = layer(x, mask=self.mask, freqs_cis=self.freqs_cis)  
         x = self.norm(x)
 
         x = self.up_projection(x)
--- src/models/r_jepa/r_jepa.py
+++ src/models/r_jepa/r_jepa.py
@@ -17,25 +17,6 @@ class RJEPA(nn.Module):
             param.requires_grad = False  # Ensure context encoder parameters are frozen
 
         # self.target_encoder = copy.deepcopy(self.context_encoder) # Since the target encoder is a deepcopy, it will also have frozen parameters.
-
-        # RoPE Frequencies 
-        head_dim = self.d_model // self.num_heads
-        freqs = torch.arange(0, head_dim, 2) / head_dim
-        freqs = 1 / (10000 ** freqs)
-
-        scale_factor = self.max_seq_len 
-
-        t = torch.arange(self.max_seq_len) / scale_factor # Divide by scale_factor to adjust the effective sequence length for RoPE
-
-        angles = torch.outer(t, freqs)  
-        freqs_cis = torch.polar(torch.ones_like(angles), angles)  
-        
-        # Causal Mask 
-        mask = torch.tril(torch.ones(self.max_seq_len, self.max_seq_len)).bool().unsqueeze(0).unsqueeze(0)
-        
-        # Register them as buffers to ensure they are moved to the correct device with the model
-        self.register_buffer('freqs_cis', freqs_cis, persistent=False)
-        self.register_buffer('mask', mask, persistent=False)
         
     def trainable_parameters(self):
         return list(self.predictor.parameters())
@@ -63,9 +44,7 @@ class RJEPA(nn.Module):
         # This is an autoregressive prediction, so we feed in the context and the previous predictions to predict the next step.
         predictions = self.predictor(
             x=target_repr[:, :-1, :], 
-            context=context_repr, 
-            mask=self.mask, 
-            freqs_cis=self.freqs_cis
+            context=context_repr
         )  # (B, reasoning_steps, encoder_dim)
 
         return predictions, target_repr
